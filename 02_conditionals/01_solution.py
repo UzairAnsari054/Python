908b510dc6ee3852f01c9ff69bf17184c09d0802
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-03-02 06:50:37 +0530

    Conditionals

---
+++ 02_conditionals/01_solution.py
@@ -0,0 +1,10 @@
+age = 25   
+
+if age < 13:
+    print("Child")
+elif age < 20:
+    print("Teenager")
+elif age < 59:
+    print("Adult")
+else:
+    print("Senior")
---
+++ 02_conditionals/04_solution.py
@@ -0,0 +1,8 @@
+color = "Green"
+
+if color == "Green":
+    print("Banana is unripe")
+elif color == "Yellow":
+    print("Banana is ripe")
+else:
+    print("Banana is overripe")
