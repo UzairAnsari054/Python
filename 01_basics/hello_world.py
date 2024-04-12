f7ec44717113fa9c69d0644afceeee0d3497e49a
Uzair Ansari <124576222+UzairAnsari054@users.noreply.github.com>
2024-04-12 14:27:57 +0530

    minor

--- 01_basics/hello_world.py
+++ 01_basics/hello_world.py
@@ -8,7 +8,7 @@
 # - memory me immutable obj create hua as 3
 # - a ke andr 3 ka reference aaya
 # Also, Value ka datatype hamesha memery me rehta h value ke saath, variable me kbhi nhi jaat h
-# mtln aap varible ko kabhi datatype assign nhi krte par memory me jo value store hoti h as a reference to that variable usse datatype assign hota h aur yeh datatype meory me hi rehta h apni value ke saath, varaible se datatype ka koi lena dena nhi h
+# mtln aap varible ko kabhi datatype assign nhi krte par memory me jo value store hoti h as a reference to that variable usse datatype assign hota h aur yeh datatype memory me hi rehta h apni value ke saath, varaible se datatype ka koi lena dena nhi h
 
 print("Hello")   
 
@@ -24,7 +24,7 @@ speak("Hello World")
 # Once an immutable object is created, its value cannot be modified.
 # eg, x=5 (this means in memory x is pointing to 5)
 # eg, x=10 (this creates a new integer object with the value 10, & that means in memory now x is pointing to 10. However 5 is still in the memory and remains unchained(Immutated). 5 continues to live in the memory as long as something is pointing at it if that's not the case then it gets garbage colleted and gets deleted from the memory but is never changed)
-# Ab hame pata h ki, agr memory me stored value ko bahare se koi point nhi krra toh garbage collector aaega aur uss value ko delete krdega, auresa hi hota h par 1 exception h ki NUMBERS AUR STRINGS ke cases me yeh garbage collection immediately nhi hota, forcefully karwa skte ho, pr immediately nhi hota. (Kyun ke maybe same number ka refrence program ko next step me chahiye ho...``)
+# Ab hame pata h ki, agr memory me stored value ko bahar se koi point nhi krra toh garbage collector aaega aur uss value ko delete krdega, aur esa hi hota h par 1 exception h ki NUMBERS AUR STRINGS ke cases me yeh garbage collection immediately nhi hota, forcefully karwa skte ho, pr immediately nhi hota. (Kyun ke maybe same number ka refrence program ko next step me chahiye ho...``)
 
 #  x = 1 
 #  y = x (Here both x and y are pointing at 1 in the memory)
@@ -32,7 +32,7 @@ speak("Hello World")
 # (this does not mean x being an interger is immutable & still its' value got changed, it just started to point on another immutable Object i.e 2, the  immutable Object 1 was never changed, it can only be deleted by garbage collector when nothing points to it )
 # Memory me 1 baar agr immutable object ban gaya, ab aap usse change nhi kr skte, haa jo variable uss immutable object ko point krra h woh immutable nhi h, usme aap dusri value rakh skte h, fir jo dusra object create ho memory me, woh variable uss new object ko point krne lagega, Phele wala immutable object change nhi hoga.
 
-# So, each object in the memory can be refrenced by multiple variables. Also, we can get the count of how many variables are reffrecing a specific object, the count is known as referenceaCount/refCount. But  sys.getrefcount() gives the same count for every string or integer
+# So, each object in the memory can be refrenced by multiple variables. Also, we can get the count of how many variables are referring a specific object, the count is known as referenceCount/refCount. But sys.getrefcount() gives the same count for every string or integer
 # sys.getrefcount("Uzair") -> 4294967295
 # sys.getrefcount("U") -> 4294967295
 
